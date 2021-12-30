81ea154e7f8198e2a574ca461414ba6ade6b2461
Saurav <pythodile@gmail.com>
2021-12-30 17:32:13 +0530

    Added the Login API along with the JWT Token

--- JWT_Login_Project/accounts/urls.py
+++ JWT_Login_Project/accounts/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import SignUpAPIView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from .views import SignUpAPIView,LoginAPI
 
 urlpatterns = [
     path('signup/', SignUpAPIView.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/',LoginAPI.as_view())
     
 ]
--- JWT_Login_Project/accounts/views.py
+++ JWT_Login_Project/accounts/views.py
@@ -3,6 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SignUpSerializer
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
+import requests,json
 
 # Create your views here.
 
@@ -25,9 +28,32 @@ class SignUpAPIView(APIView):
 
     
 
+class LoginAPI(APIView):
 
+    def get(self,request):
+        return Response({'Message':'This is get method of Login API'},status =  status.HTTP_200_OK)
+
+    def post(self, request):
+        try:
+            input_data =  request.data
+            username =  input_data.get('username')
+            password =  input_data.get('password')
+
+            user = authenticate(username = username,password = password)
+            if user is not None:
+                login(request,user)
+
+                url =  'http://localhost:8000'+reverse('token_obtain_pair')
+                data = {'username':username,'password':password}
+                tokens =  requests.post(url,data=data)
+                
+                return Response({'Tokens':json.loads(tokens.content)},status = status.HTTP_200_OK)
+            else:   
+                return Response({'Message':'Invalid username and password combination'}, status =  status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            return Response({'Message':'Something went wrong due to {}'.format(str(e))}, status = status.HTTP_400_BAD_REQUEST)
         
-    
-        
+      
 
 
