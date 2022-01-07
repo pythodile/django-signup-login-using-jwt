85b9269da2f144c07aa23094715052004a1ddec0
Saurav <pythodile@gmail.com>
2022-01-07 11:28:47 +0530

    Added the Login and Test API using Token mechanism

--- JWT_Login_Project/accounts/urls.py
+++ JWT_Login_Project/accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import SignUpAPIView
+from .views import SignUpAPIView,TokenAuthenticationView,TestAPI
+from rest_framework.authtoken import views
+
 
 urlpatterns = [
     path('signup/', SignUpAPIView.as_view()),
+    path('get-token/',views.obtain_auth_token,name='get-token'),
+    path('login/',TokenAuthenticationView.as_view(), name = 'login'),
+    path('test/',TestAPI.as_view())
     
 ]
--- JWT_Login_Project/accounts/views.py
+++ JWT_Login_Project/accounts/views.py
@@ -3,6 +3,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SignUpSerializer
+from django.contrib.auth import authenticate,login
+from django.urls import reverse
+from rest_framework.permissions import IsAuthenticated
+import requests,json
 
 # Create your views here.
 
@@ -24,6 +28,35 @@ class SignUpAPIView(APIView):
             return Response({'Message':'Something Failed due to {}'.format(str(e))}, status = status.HTTP_400_BAD_REQUEST)
 
     
+class TokenAuthenticationView(APIView):
+
+    def post(self,request):
+        try:
+            input_data =  request.data
+            username =  input_data.get('username')
+            password =  input_data.get('password')
+
+            user = authenticate(username = username,password = password)
+            if user is not None:
+                login(request,user)
+                
+                url =  'http://localhost:8000'+reverse('get-token')
+                data = {'username':username,'password':password}
+                tokens =  requests.post(url,data=data)
+
+                return Response({'Tokens':json.loads(tokens.content)},status = status.HTTP_200_OK)
+            else:   
+                return Response({'Message':'Invalid username and password combination'}, status =  status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            return Response({'Message':'Something went wrong due to {}'.format(str(e))}, status = status.HTTP_400_BAD_REQUEST)
+
+
+class TestAPI(APIView):
+    permisson_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        return Response({'Message': 'This is a test API'}, status =  status.HTTP_200_OK)
 
 
         
