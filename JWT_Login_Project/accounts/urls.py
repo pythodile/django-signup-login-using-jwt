cc548d6119a635ebb3c96b65c521b88261342127
codebadolo <122725139+codebadolo@users.noreply.github.com>
2023-10-06 17:39:20 +0000

    Update urls.py

--- JWT_Login_Project/accounts/urls.py
+++ JWT_Login_Project/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SignUpAPIView,TokenAuthenticationView,TestAPI
 from rest_framework.authtoken import views
 
-
+# after finding the  registering  
 urlpatterns = [
     path('signup/', SignUpAPIView.as_view()),
     path('get-token/',views.obtain_auth_token,name='get-token'),
